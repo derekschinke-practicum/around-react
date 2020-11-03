d39064dce00c1e668d5e701c5fd5d34857c92411
Derek Schinke <derekschinke@me.com>
2020-11-02 22:52:18 -0800

    step 6: connecting to the API, part 1: port the API module

---
+++ src/utils/utils.js
@@ -0,0 +1,17 @@
+export const nameSelector = '.profile__name';
+export const jobSelector = '.profile__job';
+export const avatarSelector = '.profile__avatar';
+export const placesListSelector = '.places__list';
+export const formValidationSettings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible',
+};
+export const editProfilePopupSelector = '.popup_type_edit';
+export const editAvatarPopupSelector = '.popup_type_edit-avatar';
+export const addCardPopupSelector = '.popup_type_add';
+export const deleteCardPopupSelector = '.popup_type_delete';
+export const imagePopupSelector = '.popup_type_image';
